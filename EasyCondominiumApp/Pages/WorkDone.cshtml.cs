5e2e43370ba1a1d687cd250b8ae561b2d41455a1
allenyo <superalstudio@outlook.com>
2023-06-22 20:37:34 +0400

    update

---
+++ EasyCondominiumApp/Pages/WorkDone.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EasyCondominiumApp.Pages
+{
+    public class WorkDoneModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ EasyCondominiumApp/Pages/statement.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EasyCondominiumApp.Pages
+{
+    public class statementModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
